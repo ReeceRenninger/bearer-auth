0405ddf6dc6045ddfc95b4af39374a04650ff9d4
ReeceRenninger <renningerreece@gmail.com>
2023-05-23 11:45:17 -0700

    set up to be tested as of now

--- src/auth/models/user.js
+++ src/auth/models/user.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('users', {
     // the big diff:  notice there is no return
@@ -9,9 +12,20 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       unique: true,
     },
-    password: {
+    password: { // DO NOT INCLUDE PASSWORD IN YOUR TOKEN
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    token: {
+      type: DataTypes.VIRTUAL,
+      //when I access the user data, this get method will generate the virtual token AT THAT MOMENT
+      //!! expiresIn math below holds token for 1 week, 1 second, times 60 to equal a minute, then 60 for an hour, 24 hours a day, 7 days.
+      get() {
+        return jwt.sign({username: this.username}, SECRET, { expiresIn: 1000 * 60 * 60 * 24 * 7});
+      },
+      set() {
+        return jwt.sign({username: this.username}, SECRET, { expiresIn: 1000 * 60 * 60 * 24 * 7});
+      },
     },
   });
   // hey, this middleware exists!   I can interact with the user before creating the record in our DB
